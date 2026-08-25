d150a8d3d734febadfec1666018552c9978669ea
Andeilso Alves <andeilsoalves@gmail.com>
2026-08-25 19:02:23 -0300

    findById implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.time.LocalDate;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     
     public static void main(String[] args) {
-        Department obj = new Department(1, "TI");
-        Seller seller = new Seller(1, "Bob", "Bob@email.com", LocalDate.now(), 3000.00, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        
-        System.out.println(obj);
-        System.out.println(seller);
+
+        Seller sellerTrue = sellerDao.findById(1);
+
+        System.out.println(sellerTrue);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,22 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.List;
-
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller seller) {
@@ -27,8 +38,43 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public Seller findById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                "SELECT seller.*, department.Name as DepName "
+                + "FROM seller INNER JOIN department "
+                + "ON seller.DepartmentId = department.Id "
+                + "Where seller.id = ?", ResultSet.TYPE_FORWARD_ONLY);
+    
+            st.setInt(1, id);
+            rs = st.executeQuery();
+    
+            if(rs.next()){
+                Department department = new Department(
+                    rs.getInt("DepartmentId"), 
+                    rs.getString("DepName")
+                );
+    
+                return new Seller(
+                    rs.getInt("Id"),
+                    rs.getString("Name"),
+                    rs.getString("Email"),
+                    LocalDate.parse(rs.getDate("BirthDate").toString()),
+                    rs.getDouble("BaseSalary"),
+                    department
+                );
+            };
+    
+            throw new DbException("Seller not found!");
+        } catch (Exception e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeConnection();
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
