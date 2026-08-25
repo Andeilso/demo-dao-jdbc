16cb265c846165d8d63cd32335abb7f1e84a6cfd
Andeilso Alves <andeilsoalves@gmail.com>
2026-08-25 15:07:57 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.time.LocalDate;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     
     public static void main(String[] args) {
         Department obj = new Department(1, "TI");
+        Seller seller = new Seller(1, "Bob", "Bob@email.com", LocalDate.now(), 3000.00, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
